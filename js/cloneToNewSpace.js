03f3e7caabcd1ec7f8c9eaf087abaaf76a1c8df7
Andrew Bresee <andrew.bresee@smartthings.com>
2017-02-09 10:15:02 -0800

    Allow for deleting, recreating, and uploading content to a new space

--- js/cloneToNewSpace.js
+++ js/cloneToNewSpace.js
@@ -2,6 +2,7 @@ import config from  '~/config.json';
 import ContentfulManagement from 'contentful-management';
 import spaceExport from 'contentful-export';
 import spaceImport from 'contentful-import';
+import chalk from 'chalk';
 
 
 let cloneToNewSpace = () => {
@@ -9,36 +10,89 @@ let cloneToNewSpace = () => {
     accessToken: config.uploadToContentfulManagementToken
   })
 
-  let options = {
+  let cloneFromOptions = {
     spaceId: config.cloneFromContentfulSpace,
     managementToken: config.uploadToContentfulManagementToken,
   }
 
-  spaceExport(options)
-  .then((output) => {
-    client.createSpace({name: "New Test Space"})
+  let uploadSpaceName = 'New Test Space'
+
+  // *** Where everthing is kicked off ***
+  client.getSpaces()
+  .then((allSpaces) => {
+    let uploadSpaceExists = false;
+    let uploadSpaceID
+    for (let space of allSpaces.items) {
+      console.log('spaces found: ', space.name)
+      if (space.name === uploadSpaceName) {
+        uploadSpaceExists = true;
+        uploadSpaceID = space.sys.id
+      }
+    }
+    if (uploadSpaceExists) {
+      console.log(chalk.green('\n Upload space with the name - '+uploadSpaceName+' - exists, deleting it now \n'))
+      deleteAndRecreateUploadSpace(uploadSpaceID)
+    } else {
+      console.log(chalk.green('\n Upload space with the name - '+uploadSpaceName+'- does not exists. Making a new one. \n'))
+      createNewSpace()
+    }
+  })
+
+  // Makes a new space with the specified uploadSpaceName
+  let createNewSpace = () => {
+    client.createSpace({name: uploadSpaceName})
     .then((createdSpace) => {
-      console.log(' createdSpace: ', createdSpace);
+      console.log(chalk.green('\n New space with the name - '+uploadSpaceName+' - has been created. \n'))
+      uploadContentToNewSpace(createdSpace)
+    })
+    .catch((err) => {
+      console.log('Error in createSpaceMembershipWithId: ', err)
+    })
+  }
+
+  // With an existing space, this will delete it, then call createNewSpace()
+  let deleteAndRecreateUploadSpace = (uploadSpaceID) => {
+    client.getSpace(uploadSpaceID)
+    .then((uploadSpace) => {
+      uploadSpace.delete()
+      .then(() => {
+        console.log(chalk.green('\n Exisiting space successfully deleted \n'))
+        createNewSpace()
+      })
+      .catch((err) => {
+        console.log(chalk.red(' Error in deleting the space:', err))
+      })
+    })
+    .catch((err) => {
+      console.log(chalk.red('Err in retrieving upload space to delete: ', err));
+    })
+  }
+
+  // Will upload cloned content to newly created space
+  let uploadContentToNewSpace = (newSpace) => {
+    spaceExport(cloneFromOptions)
+    .then((output) => {
+      console.log(chalk.green('\n Cloned space content found \n'))
       let uploadOptions = {
         content: output,
-        spaceId: createdSpace.sys.id,
+        spaceId: newSpace.sys.id,
         managementToken: config.uploadToContentfulManagementToken
       }
       spaceImport(uploadOptions)
       .then((finalOutput) => {
-        console.log('Data Imported successfully')
+        console.log(chalk.green('\n *** All Data Imported successfully *** \n'))
       })
       .catch((err) => {
-        console.log('Error in uploading the space: ', err)
+        console.log(chalk.red('Error in uploading the space: ', err))
       })
     })
     .catch((err) => {
-      console.log(' Error in creating a new space:', err)
+      if (err.message = "The resource could not be found.") {
+        console.log(chalk.red('The space could not be found. Making a new one'));
+        uploadNewContent()
+      }
     })
-  })
-  .catch((err) => {
-    console.log(' Error in downloading the space: ', err);
-  })
+  }
 
 }
 
